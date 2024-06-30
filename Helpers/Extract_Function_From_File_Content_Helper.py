2594877cbf72b451b310f645dcf626ecdd53a71e
quocbui2020 <quocbui2010@yahoo.com>
2024-06-30 00:50:18 -0500

    save progress

--- Helpers/Extract_Function_From_File_Content_Helper.py
+++ Helpers/Extract_Function_From_File_Content_Helper.py
@@ -58,7 +58,7 @@ class ExtractFunctionFromFileContentHelper:
         py_content_without_comment = self.remove_python_comments(content)
         
         # Regular expression to match lines starting with optional spaces followed by "def"
-        pattern = re.compile(r'^(\s*def .*)$', re.MULTILINE)
+        pattern = re.compile(r'^(\s*def .*)$', re.MULTILINE) # ^: beginning of the line; $: end of the line.
 
         # Find all matches for lines starting with "def"
         matches = list(pattern.finditer(py_content_without_comment))
@@ -70,6 +70,84 @@ class ExtractFunctionFromFileContentHelper:
         # Split the content into sections based on the positions
         content_blocks = [py_content_without_comment[positions[i]:positions[i+1]] for i in range(len(positions)-1)]
         
+        # For each content block, determine the end of the function:
+        for block in content_blocks:
+            # Split the block by '\n'
+            block_lines = block.split('\n')
+
+            # Collect all lines associates to the functions:
+            function_lines = []
+            keep_next_line = False # This flag indicates that the next line belongs to the function. Handle case when the line is a part of string.
+            spaces_before_def_count = -1
+
+            tracker = {
+                "missing_closed_curly_brackets": 0,
+                "missing_closed_squared_brackets": 0,
+                "missing_closed_parentheses": 0,
+                "missing_single_quote": False,
+                "missing_double_quote": False,
+                "last_char_backward_slash": False,
+            }
+
+            # Iterate through each line of a block. Skip i=0 because it is just ''
+            for i in range(1, len(block_lines)):
+                inside_leading_white_space_region = True
+                # Count the leading white spaces of each line and track te bracket
+                leading_spaces_count = 0
+                for index, c in enumerate(block_lines[i]):
+                    # Check if the character is leading white space
+                    if inside_leading_white_space_region and c == ' ':
+                        leading_spaces_count += 1
+                    elif inside_leading_white_space_region and c != '':
+                        inside_leading_white_space_region = False
+
+                    inside_comment = tracker['missing_single_quote'] or tracker['missing_double_quote']
+                    
+                    if c == '"' and not tracker['missing_single_quote']:
+                        tracker['missing_double_quote'] = -tracker['missing_double_quote']
+                    elif c == "'" and not tracker['missing_doubl_quote']:
+                        tracker['missing_single_quote'] = -tracker['missing_single_quote']
+                    elif not inside_comment and c == '{':
+                        tracker['missing_closed_curly_brackets'] += 1
+                    elif not inside_comment and c == '}':
+                        tracker['missing_closed_curly_brackets'] -= 1
+                    elif not inside_comment and c == '[':
+                        tracker['missing_closed_squared_brackets'] += 1
+                    elif not inside_comment and c == ']':
+                        tracker['missing_closed_squared_brackets'] -= 1
+                    elif not inside_comment and c == '(':
+                        tracker['missing_closed_parentheses'] += 1
+                    elif not inside_comment and c == ')':
+                        tracker['missing_closed_parentheses'] -= 1
+                    elif not inside_comment and index == len(block_lines[i]) - 1 and c == '\\':  # last character in the line is backward slash
+                        tracker['last_char_backward_slash'] = True
+                    else:
+                        break
+                
+                # Decide if this line a part of the function or not:
+                if i >= 2:
+                    if (leading_spaces_count > spaces_before_def_count) or (keep_next_line == True):
+                        function_lines.append(block_lines[i])
+
+                        # Determine `keep_next_line` flag:
+                        if tracker['missing_closed_curly_brackets'] or tracker['missing_closed_parentheses'] or tracker['missing_closed_squared_brackets'] or tracker['last_char_backward_slash'] or tracker['missing_single_quote'] or tracker['missing_double_quote']:
+                            keep_next_line = True
+                        else:
+                            keep_next_line = False
+                    else:
+                        break # The line is not the part of the function, break out of block line loop.
+
+                elif i == 1: # Assuming if i=1, it contains keyword `def` to define a python function.
+                    function_lines.append(block_lines[i])
+                    spaces_before_def_count = leading_spaces_count
+
+                
+            
+            # Set block = all function lines.
+            block = '\n'.join(function_lines)
+
+
+
         return content_blocks
 
     ## C++ ##
@@ -106,6 +184,7 @@ testing_content = '''
 def functionA():
     inside function A.
         inside function A.
+
     inside function A.
 
 statement outside of the function.
