0280014da841c5545d7e8d896cf64dc5d77c3559
quocbui2020 <quocbui2010@yahoo.com>
2024-09-30 08:10:17 -0500

    switch to use the command line input arguments to run scraper

--- Bugzilla/Bugzilla_mozilla_file_function_scraper.py
+++ Bugzilla/Bugzilla_mozilla_file_function_scraper.py
@@ -494,19 +494,20 @@ class Mozilla_File_Function_Scraper:
 #########################################################################
 
 if __name__ == "__main__":
-    # parser = argparse.ArgumentParser(description="")
-    # parser.add_argument('arg_1', type=int, help='Argument 1')
-    # parser.add_argument('arg_2', type=int, help='Argument 2')
-    # parser.add_argument('arg_3', type=int, help='Argument 3')
-    # parser_args = parser.parse_args()
-    # task_group = parser_args.arg_1
-    # start_row = parser_args.arg_2
-    # end_row = parser_args.arg_3
+    parser = argparse.ArgumentParser(description="")
+    parser.add_argument('arg_1', type=int, help='Argument 1')
+    parser.add_argument('arg_2', type=int, help='Argument 2')
+    parser.add_argument('arg_3', type=int, help='Argument 3')
+    parser_args = parser.parse_args()
+    task_group = parser_args.arg_1
+    start_row = parser_args.arg_2
+    end_row = parser_args.arg_3
 
     # Testing specific input arguments:
-    task_group = 1   # Task group
-    start_row = 1108   # Start row
-    end_row = 1108   # End row
+    # task_group = 1   # Task group
+    # start_row = 1108   # Start row
+    # end_row = 1108   # End row
+    
     scraper = Mozilla_File_Function_Scraper()
     scraper.run_scraper(task_group, start_row, end_row)
 
